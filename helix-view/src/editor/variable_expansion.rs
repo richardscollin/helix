9b5758c5e6ab3fc73b019a90dd861e22254fd7e6
Collin Richards <richardscollin@gatech.edu>
2023-09-12 03:38:18 +0800

    begin refactor variable expansion

--- helix-view/src/editor/variable_expansion.rs
+++ helix-view/src/editor/variable_expansion.rs
@@ -1,8 +1,94 @@
 use std::borrow::Cow;
+use log::warn;
 
-use crate::Editor;
+use crate::{Editor, Document, View};
+
+fn expand_one_variable(view: &View, doc: &Document, variable: &str) -> anyhow::Result<String> {
+    match variable {
+        "basename" => Ok(doc
+            .path()
+            .and_then(|it| it.file_name().and_then(|it| it.to_str()))
+            .unwrap_or(crate::document::SCRATCH_BUFFER_NAME)
+            .to_owned()),
+        "filename" => Ok(doc
+            .path()
+            .and_then(|it| it.to_str())
+            .unwrap_or(crate::document::SCRATCH_BUFFER_NAME)
+            .to_owned()),
+        "dirname" => Ok(doc
+            .path()
+            .and_then(|p| p.parent())
+            .and_then(std::path::Path::to_str)
+            .unwrap_or(std::env::current_dir()?.to_str().unwrap())
+            .to_owned()),
+        "linenumber" => Ok((doc
+            .selection(view.id)
+            .primary()
+            .cursor_line(doc.text().slice(..)) + 1)
+            .to_string()),
+        "selection" => Ok(doc
+            .selection(view.id)
+            .primary()
+            .fragment(doc.text().slice(..))
+            .to_string()),
+        _ => anyhow::bail!("Unknown variable"),
+    }
+}
+
 
 pub fn expand_variables<'a>(editor: &Editor, input: &'a str) -> anyhow::Result<Cow<'a, str>> {
+    if input.find('%').is_none() {
+        // no expansion in this case
+
+        // this special case handling could be removed
+        return Ok(std::borrow::Cow::Borrowed(input));
+    }
+
+    let (view, doc) = current_ref!(editor);
+    let _shell = &editor.config().shell;
+
+    const RESERVE_CAPACITY: usize = 32; // extra capacity for expected expansion memory
+    let mut output: String = String::with_capacity(input.len() + RESERVE_CAPACITY);
+
+    const SH_VAR_PREFIX: &str = "%sh{";
+    const VAR_PREFIX: &str = "%{";
+
+    let mut remaining = input;
+    while !remaining.is_empty() {
+        if remaining.starts_with(SH_VAR_PREFIX) {
+            remaining = &remaining[SH_VAR_PREFIX.len()..];
+            // TODO
+
+        } else if remaining.starts_with(VAR_PREFIX) {
+            remaining = &remaining[VAR_PREFIX.len()..];
+            if let Some(closing_brace_location) = remaining.find('}') {
+                let expanded = expand_one_variable(view, doc, &remaining[..closing_brace_location]);
+                output.push_str(expanded?.trim());
+                remaining = &remaining[(closing_brace_location+1)..];
+            } else {
+                // missing closing brace
+                warn!("missing closing brace");
+                output.push_str(remaining);
+                break;
+            }
+        } else if remaining.starts_with('%') {
+            // if we didn't match above
+            // consume a single char and append to output
+            output.push('%');
+            remaining = &remaining[1..];
+        } else {
+            // scan until next %
+            let loc = remaining.find('%').unwrap_or(remaining.len());
+            output.push_str(&remaining[..loc]);
+            remaining = &remaining[loc..];
+        }
+    }
+
+    Ok(std::borrow::Cow::Owned(output))
+}
+
+/*
+pub fn expand_variables2<'a>(editor: &Editor, input: &'a str) -> anyhow::Result<Cow<'a, str>> {
     let (view, doc) = current_ref!(editor);
     let shell = &editor.config().shell;
 
@@ -25,36 +111,7 @@ pub fn expand_variables<'a>(editor: &Editor, input: &'a str) -> anyhow::Result<C
                                 o.push_str(&input[last_push_end..index]);
                                 last_push_end = end + 1;
 
-                                let value = match &input[index + 2..end] {
-                                    "basename" => doc
-                                        .path()
-                                        .and_then(|it| it.file_name().and_then(|it| it.to_str()))
-                                        .unwrap_or(crate::document::SCRATCH_BUFFER_NAME)
-                                        .to_owned(),
-                                    "filename" => doc
-                                        .path()
-                                        .and_then(|it| it.to_str())
-                                        .unwrap_or(crate::document::SCRATCH_BUFFER_NAME)
-                                        .to_owned(),
-                                    "dirname" => doc
-                                        .path()
-                                        .and_then(|p| p.parent())
-                                        .and_then(std::path::Path::to_str)
-                                        .unwrap_or(std::env::current_dir()?.to_str().unwrap())
-                                        .to_owned(),
-                                    "linenumber" => (doc
-                                        .selection(view.id)
-                                        .primary()
-                                        .cursor_line(doc.text().slice(..))
-                                        + 1)
-                                    .to_string(),
-                                    "selection" => doc
-                                        .selection(view.id)
-                                        .primary()
-                                        .fragment(doc.text().slice(..))
-                                        .to_string(),
-                                    _ => anyhow::bail!("Unknown variable"),
-                                };
+                                //vlet value = expand(&doc, &input[index + 2..end]);
 
                                 o.push_str(value.trim());
 
@@ -136,3 +193,4 @@ pub fn expand_variables<'a>(editor: &Editor, input: &'a str) -> anyhow::Result<C
         None => Ok(std::borrow::Cow::Borrowed(input)),
     }
 }
+*/
